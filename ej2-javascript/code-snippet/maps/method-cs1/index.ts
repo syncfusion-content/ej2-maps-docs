a5014a6bdd2fa3d3e90b41e1281edd1caca60004
Indumathi <indumathi.ravi@syncfusion.com>
2023-12-21 16:43:04 +0530

    856466: added content and samples for Volume 4 Release.

---
+++ ej2-javascript/code-snippet/maps/method-cs1/index.ts
@@ -0,0 +1,63 @@
+
+
+
+import { Maps, Zoom, Marker, IMinMaxLatitudeLongitude } from '@syncfusion/ej2-maps';
+import { world_map } from './world-map.ts';
+Maps.Inject(Zoom, Marker);
+let maps: Maps = new Maps({
+    zoomSettings: {
+        enable: true,
+        zoomFactor: 7
+    },
+    centerPosition: {
+        latitude: 21.815447,
+        longitude: 80.1932
+    },
+    layers: [
+        {
+            shapeData: world_map,
+            markerSettings: [
+                {
+                    visible: true,
+                    height: 25,
+                    width: 25,
+                    shape: 'Circle',
+                    animationDuration: 1500,
+                    dataSource: [
+                        {
+                            latitude: 22.572646,
+                            longitude: 88.363895
+                        },
+                        {
+                            latitude: 25.0700428,
+                            longitude: 67.2847875
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+});
+maps.appendTo('#element');
+if (document.getElementById('button') != null) {
+    document.getElementById('button').onclick = () => {
+        let mapBoundCoordinates: IMinMaxLatitudeLongitude;
+        mapBoundCoordinates = maps.getMinMaxLatitudeLongitude();
+        const displayDiv = document.getElementById('coordinatesDisplay');
+        displayDiv.innerHTML = '';
+        if (mapBoundCoordinates) {
+            for (const key in mapBoundCoordinates) {
+                if (Object.hasOwnProperty.call(mapBoundCoordinates, key)) {
+                    const p = document.createElement('p');
+                    p.textContent = `${key}: ${mapBoundCoordinates[key]}`;
+                    displayDiv.appendChild(p);
+                }
+            }
+        } else {
+            displayDiv.textContent = 'No coordinates available';
+        }
+    };
+}
+
+
+
---
+++ ej2-javascript/maps/js/methods.md
@@ -0,0 +1,31 @@
+---
+layout: post
+title: Methods in ##Platform_Name## Maps control | Syncfusion
+description: Learn here all about Methods in Syncfusion ##Platform_Name## Maps control of Syncfusion Essential JS 2 and more.
+platform: ej2-javascript
+control: Methods 
+publishingplatform: ##Platform_Name##
+documentation: ug
+domainurl: ##DomainURL##
+---
+
+# Methods in ##Platform_Name## Maps control
+
+## Methods
+
+This section explains the methods used in the Maps control.
+
+## getMinMaxLatitudeLongitude
+
+`getMinMaxLatitudeLongitude` method is used to get the minimum and maximum latitude and longitude values of the visible area of the Maps. This method returns `IMinMaxLatitudeLongitude`, which provides the minimum and maximum latitude and longitude coordinates of the Maps.
+
+{% tabs %}
+{% highlight js tabtitle="index.js" %}
+{% include code-snippet/maps/method-cs1/index.js %}
+{% endhighlight %}
+{% highlight html tabtitle="index.html" %}
+{% include code-snippet/maps/method-cs1/index.html %}
+{% endhighlight %}
+{% endtabs %}
+        
+{% previewsample "page.domainurl/code-snippet/maps/method-cs1" %}
